630b4bd505a919b3b2af5ec2f26adf54134998fe
Jesse Piccolo <jpiccolo@frontlineed.com>
2017-04-07 08:52:49 -0400

    Windows version updated to 1.1.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,6 +8,6 @@
 #
 
 default['dotnetcore']['package']['name'] = 'dotnet-dev-1.0.0-preview2-003131'
-default['dotnetcore']['package']['source_url'] = 'https://download.microsoft.com/download/5/5/7/557D02A5-C3D0-4EF6-A570-4F75CD0DA5BF/DotNetCore.1.0.1-WindowsHosting.exe'
+default['dotnetcore']['package']['source_url'] = 'https://download.microsoft.com/download/9/5/1/95198156-644E-4CCE-8DA1-C41F7658510C/dotnet-win-x64.1.1.1.exe'
 default['dotnetcore']['package']['tar'] = 'https://go.microsoft.com/fwlink/?LinkID=827529'
 default['dotnetcore']['apt_package_source'] = 'http://apt-mo.trafficmanager.net/repos/dotnet-release'
